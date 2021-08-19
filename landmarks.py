9c4cfb1146a225497acdb1f189deac207db79adc
Morteza Nedaei <nedaei1375@gmail.com>
2021-08-19 12:58:14 +0430

    add Dlib landmark positions

---
+++ landmarks.py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+
+# define a dictionary that maps the indexes of the facial
+# landmarks to specific face regions
+FACIAL_LANDMARKS_IDXS = OrderedDict([
+    ("mouth", (48, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 35)),
+    ("jaw", (0, 17))
+])
